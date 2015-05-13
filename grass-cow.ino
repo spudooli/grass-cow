bdf92d68561b13e86f1815430c174ced03a00e64
Dave <dave@spudooli.com>
2015-05-13 17:00:47 +1200

    Add in the Ethernet shield code

--- grass-cow.ino
+++ grass-cow.ino
@@ -10,6 +10,28 @@
 /*****************************************************************************/
 
 #include "Arduino.h"
+include <Ethernet.h>
+// the sensor communicates using SPI, so include the library:
+#include <SPI.h>
+
+
+// assign a MAC address for the ethernet controller.
+// fill in your address here:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+// assign an IP address for the controller:
+IPAddress ip(192, 168, 1, 20);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+
 
 class Ultrasonic
 {
@@ -52,10 +74,26 @@ long Ultrasonic::microsecondsToInches(void)
 Ultrasonic ultrasonic(7);
 void setup()
 {
-	Serial.begin(9600);
+  Serial.begin(9600);
+
+  // start the SPI library:
+  SPI.begin();
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  
+  // give the sensor and Ethernet shield time to set up:
+  delay(1000);
+  
+  
 }
 void loop()
 {
+  
+  // listen for incoming Ethernet connections:
+  listenForEthernetClients();
+  
 	long RangeInInches;
 	long RangeInCentimeters;
 	ultrasonic.DistanceMeasure();// get the current signal time;
@@ -68,3 +106,46 @@ void loop()
 	Serial.println(" cm");
 	delay(100);
 }
+
+void listenForEthernetClients() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("Got a client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println();
+          // print the current readings, in HTML format:
+          client.print("Grass range: ");
+          client.print(RangeInCentimeters);
+          client.println("<br />");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+  }
+}
+
+
